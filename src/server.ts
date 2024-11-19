515549979dac8d9e33cca35baef12d17cc8cfe09
Sarthak vyas <142726436+Sarthak-vyas183@users.noreply.github.com>
2024-11-19 14:47:17 +0530

    lint stage test - 3

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,10 @@
 function welcome(name : string) {
     console.log(`welcome ${name}`);
+    const user = {
+        name : 'vyas'
+    } 
+    const fname = user['name'];
+    return name + fname;
 }
 
 welcome("sarthak");
