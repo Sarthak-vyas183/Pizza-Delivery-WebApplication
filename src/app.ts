9c815c3b5958304848eb8a53eb0424adc5eea323
Sarthak vyas <142726436+Sarthak-vyas183@users.noreply.github.com>
2024-11-19 21:53:35 +0530

    Error Handling setup done

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,30 @@
-import express  from "express";
+import logger from "./logger";
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import createHttpError, { HttpError } from "http-errors";
+import express, { Request, Response, NextFunction } from "express";
 const app = express()
 
 app.get("/", (req, res) => {
+    // const err = createHttpError(401, "you can not access error");
+    // throw err;
     res.send("welcome to pizza hub");
 })
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+app.use((err : HttpError, req : Request, res : Response, next : NextFunction)=>{
+       logger.error(err.message);
+       const statusCode = err.statusCode || 500;
+
+       
+       res.status(statusCode).json({
+          error : [
+             {
+                type : err.name,
+                msg : err.message,
+                path : '',
+                location : ''
+             },
+          ],
+       })
+})
 export default app;
