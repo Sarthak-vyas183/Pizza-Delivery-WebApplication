1ec78ed914a38290ff66d98de220b7adce430930
Sarthak vyas <142726436+Sarthak-vyas183@users.noreply.github.com>
2024-11-19 17:00:56 +0530

    express setUp

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,16 @@
 import {Config} from './config/index'
+import app from './app';
 
-console.log(Config.PORT);
-console.log(Config.API)
+const startServer = () => {
+    const PORT = Config.PORT;
+    try {
+        app.listen(PORT, () => {
+            console.log(`Server is listing on port ${PORT}......`)
+        })
+    } catch (error) {
+        console.log(error)
+        process.exit(1)
+    }
+}
+
+startServer()
